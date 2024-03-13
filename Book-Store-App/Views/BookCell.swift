02738b01e46b37db51a3316216c327762e10bef0
malicelebii <mehmetalicelebii@gmail.com>
2024-03-13 14:49:49 +0300

    + BookCell file added

---
+++ Book-Store-App/Views/BookCell.swift
@@ -0,0 +1,18 @@
+//
+//  BookCell.swift
+//  Book-Store-App
+//
+//  Created by Mehmet Ali ÇELEBİ on 8.03.2024.
+//
+
+import UIKit
+
+class BookCell: UICollectionViewCell {
+    @IBOutlet weak var bookImage: UIImageView!
+    
+    @IBOutlet weak var bookName: UILabel!
+    
+    @IBOutlet weak var bookAuthor: UILabel!
+    
+    @IBOutlet weak var bookYear: UILabel!
+}
